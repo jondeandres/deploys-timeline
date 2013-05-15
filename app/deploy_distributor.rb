7fb68a31c33233c93f494e36d2399e0cd995dbf6
Jon de Andres <jondeandres@gmail.com>
2013-05-16 01:16:31 +0200

    Calc all the possible sets to push deploy ids in them.

--- app/deploy_distributor.rb
+++ app/deploy_distributor.rb
@@ -1,8 +1,23 @@
 class DeployDistributor
   attr_reader :deploy
 
+  def self.criterias
+    @criterias ||= [:country, :project, :environment]
+  end
+
+  def self.combinations
+    @combinations ||= (1..criterias.length).inject([]) do  |acc, i|
+      acc += criterias.combination(i).to_a
+      acc
+    end
+  end
+
   def initialize(deploy)
     @deploy = deploy
+  end
+
+  def combinations
+    self.class.combinations
   end
 
   def distribute
@@ -15,17 +30,16 @@ class DeployDistributor
     @redis ||= ::Redis.new
   end
 
-  def set_for(project, environment)
-    set = ["deploys"]
-    set << project if project
-    set << environment if environment
-    set.join(":")
-  end
-
   def sets_to_enqueue
-    [set_for(nil, nil),
-     set_for(deploy.project, nil),
-     set_for(deploy.project, deploy.environment),
-     set_for(nil, deploy.environment)]
+    combinations.inject([]) do |sets, combination|
+      if combination.map{|attr| deploy.send(attr) }.all?
+        sets << combination.inject("deploys") do |acc, attr|
+          value = deploy.send(attr)
+          acc += ":#{attr}:#{value}"
+          acc
+        end
+      end
+      sets
+    end
   end
 end
