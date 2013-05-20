cf9a2fd727c9e9723842b6691da2728f34bf2d32
Jon de Andres <jondeandres@gmail.com>
2013-05-20 18:27:46 +0200

    Split app.js into multiple js files.

--- public/js/app.js
+++ public/js/app.js
@@ -11,35 +11,3 @@ Deploys.config(["$routeProvider", function($routeProvider) {
 		when('/project/:project', { templateUrl: '/templates/deploys/list.html', controller: DeployListCtrl }).
 		when('/environment/:environment', { templateUrl: '/templates/deploys/list.html', controller: DeployListCtrl });
 }]);
-
-Deploys.directive("deployFilters", function() {
-	return {
-		restrict: 'E',
-		templateUrl: '/templates/deploys/filters.html',
-		controller: DeployFiltersCtrl
-	};
-});
-
-function DeployFiltersCtrl($scope, $http) {
-	$http.get('/filters').success(function(data) {
-		$scope.countries = data.countries;
-		$scope.environments = data.environments;
-		$scope.projects = data.projects;
-	});
-
-	$scope.changePath = function() {
-
-	};
-}
-
-function DeployListCtrl($scope, $route, $routeParams, $http) {
-	var	render = function() {
-		// TODO: Change path depending on current hash.
-		$http.get(api_path('deploys')).success(
-			function(data) {
-				$scope.deploys = data.deploys;
-			});
-	};
-
-	render();
-};
---
+++ public/js/directives.js
@@ -0,0 +1,7 @@
+Deploys.directive("deployFilters", function() {
+	return {
+		restrict: 'E',
+		templateUrl: '/templates/deploys/filters.html',
+		controller: DeployFiltersCtrl
+	};
+});
