4bcf9b9fce44c3501a82d32eed5446b1ea5fcb71
Jon de Andres <jondeandres@gmail.com>
2013-05-20 18:25:23 +0200

    Add deploy filters.
    Actually /filters path is not implemented, so they don't work yet.

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,35 @@ function api_path(path) {
 	return "/api/" + path;
 }
 
-var Deploys = angular.module("deploys", []).
-	config(["$routeProvider", function($routeProvider) {
-		$routeProvider.
-			when('/', { templateUrl: '/templates/deploys/list.html', controller: DeployListCtrl }).
-  		when('/country/:country', { templateUrl: '/templates/deploys/list.html', controller: DeployListCtrl }).
-			when('/project/:project', { templateUrl: '/templates/deploys/list.html', controller: DeployListCtrl }).
-			when('/environment/:environment', { templateUrl: '/templates/deploys/list.html', controller: DeployListCtrl });
-	}]);
+var Deploys = angular.module("deploys", []);
+
+Deploys.config(["$routeProvider", function($routeProvider) {
+	$routeProvider.
+		when('/', { templateUrl: '/templates/deploys/list.html', controller: DeployListCtrl }).
+  	when('/country/:country', { templateUrl: '/templates/deploys/list.html', controller: DeployListCtrl }).
+		when('/project/:project', { templateUrl: '/templates/deploys/list.html', controller: DeployListCtrl }).
+		when('/environment/:environment', { templateUrl: '/templates/deploys/list.html', controller: DeployListCtrl });
+}]);
+
+Deploys.directive("deployFilters", function() {
+	return {
+		restrict: 'E',
+		templateUrl: '/templates/deploys/filters.html',
+		controller: DeployFiltersCtrl
+	};
+});
+
+function DeployFiltersCtrl($scope, $http) {
+	$http.get('/filters').success(function(data) {
+		$scope.countries = data.countries;
+		$scope.environments = data.environments;
+		$scope.projects = data.projects;
+	});
+
+	$scope.changePath = function() {
+
+	};
+}
 
 function DeployListCtrl($scope, $route, $routeParams, $http) {
 	var	render = function() {
